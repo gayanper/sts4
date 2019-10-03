a57c4a1baf7b5ae05cc17ec546e822747b4a9f87
gayanper <gayanper@gmail.com>
2019-10-03 20:57:09 +0530

    Fix for deadlock issue

--- idea-extensions/idea-boot-java/src/org/spring/tools/boot/java/ls/ApplicationUtils.java
+++ idea-extensions/idea-boot-java/src/org/spring/tools/boot/java/ls/ApplicationUtils.java
@@ -3,11 +3,18 @@ package org.spring.tools.boot.java.ls;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.util.Computable;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.Future;
+
 public final class ApplicationUtils {
     private ApplicationUtils() {
     }
 
     public static <T> T runReadAction(Computable<T> computable) {
         return ApplicationManager.getApplication().runReadAction(computable);
+    }
+
+    public static <T> Future<T> executeOnIntellijPooledThread(Callable<T> callable) {
+        return ApplicationManager.getApplication().executeOnPooledThread(callable);
     }
 }
